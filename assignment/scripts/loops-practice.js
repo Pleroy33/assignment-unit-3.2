43e0c5fcf143ce11dbed21128861d255b15a66b3
Paul LeRoy <pleroy33@gmail.com>
2023-11-10 14:10:54 -0600

    created 3 for loops

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -13,11 +13,15 @@ for (let i=0; i<4; i++) {
 // 1.a. TODO: Write a for loop to console.log the numbers from 0 to 5 
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 0 to 5');
-
+for (let i=0; i<6; i++){//the part of the example loop needs to be changed from i<4 to i<6 
+  console.log(i);// 
+}
 // 1.b. TODO: Write a for loop to console.log the numbers from 3 to 5
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 3 to 5');
-
+for (let i=3; i<6; i++){//the part of the example loop that you need to change is i=0 to i=3, and the change above.  
+  console.log(i);
+}
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this?
 console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
