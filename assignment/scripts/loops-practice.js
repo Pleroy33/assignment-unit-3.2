d3dafa8dd9528db51ab11e10b3f39dbe1b50a79a
Paul LeRoy <pleroy33@gmail.com>
2023-11-10 14:40:56 -0600

    created 2 more for loops, countup by even numbers, countdown from 5

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -6,29 +6,36 @@ console.log('---- 1. For loops ----');
 // Example: a for loop to console.log numbers from 0 to 3
 console.log('count from 0 to 3');
 // start i at 0, while i is < 4 do code between {}, afterwards add 1 to i (i++)
-for (let i=0; i<4; i++) {  
-  console.log(i);
+  for (let i=0; i<4; i++) {  
+    console.log(i);
 }
 
 // 1.a. TODO: Write a for loop to console.log the numbers from 0 to 5 
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 0 to 5');
-for (let i=0; i<6; i++){//the part of the example loop needs to be changed from i<4 to i<6 
-  console.log(i);// 
+  for (let i=0; i<6; i++){//the part of the example loop needs to be changed from i<4 to i<6 
+    console.log(i);// 
 }
 // 1.b. TODO: Write a for loop to console.log the numbers from 3 to 5
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 3 to 5');
-for (let i=3; i<6; i++){//the part of the example loop that you need to change is i=0 to i=3, and the change above.  
-  console.log(i);
+  for (let i=3; i<6; i++){//the part of the example loop that you need to change is i=0 to i=3, and the change above.  
+    console.log(i);
 }
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this?
 console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
 
+for (i=2; i<11; i++){
+  if (i % 2 === 0){
+    console.log(i);// the part that needs to be changed is the console.log.  It needs to be set to filter the results to console.log even numbers only
+  }
+}
 // 1.d. (STRETCH) TODO: Write a for loop to do a countdown from 5 to 0
 console.log('STRETCH: countdown from 5 to 0');
-
+for (let i=5; i>-1; i--){
+  console.log(i);
+}
 
 // 2. For of loops  
 console.log('---- 2. For Of loop ----');
