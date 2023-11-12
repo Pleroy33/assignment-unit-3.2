1d499daa9cddf04cbe766b067b8c6ec54a918fd9
Paul LeRoy <pleroy33@gmail.com>
2023-11-12 16:08:12 -0600

    created 3 while loops

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -33,7 +33,7 @@ for (i=2; i<11; i++){
 }
 // 1.d. (STRETCH) TODO: Write a for loop to do a countdown from 5 to 0
 console.log('STRETCH: countdown from 5 to 0');
-for (let i=5; i>-1; i--){
+for (let i=5; i>-1; i--){// for loop that counts down from 5 to 0.
   console.log(i);
 }
 
@@ -43,16 +43,33 @@ let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb']
 
 // 2.a. TODO: Write a for of loop to console.log each star in the 'stars' array
 console.log('Some stars:');
-
+for (names of stars){ //wrote a for of loop, by creating variable 'names' that uses each variable in the array 'stars'
+  console.log(names);
+}
 
 // 3. While loops  
 console.log('---- 3. While loop ----');
 
 // 3.a. TODO: Write a while loop to console.log each star in the 'stars' array
 console.log('Some stars using while:');
+let x = 0; // created variable x 
+while (stars [x]){  // write a while loop where x is the index of the stars array strings
+    console.log(stars [x] );// starting at index 0 log each string in stars
+      x++;// increment the index of the stars array string by 1
+ 
+}
 
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
 console.log('count from 0 to 5');
-
+let number = 0  // create a variable called number
+while (number < 6){// create a while loop, while number is less than 6, log it
+  console.log(number);
+    number++;// increment number by 1 
+}
 // 3.c. TODO: Write a while loop to console.log the numbers from 10 to 5
 console.log('count backwards from 10 to 5');
+let numbers =10 //created a new variable called 'numbers'
+  while (numbers > 4) {// wrote a while loop where numbers is greater than 4 to log it.
+    console.log (numbers);
+      numbers --;//decreased numbers by 1,  
+  }
